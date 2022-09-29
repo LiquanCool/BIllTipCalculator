9abe1c74c9208a39a6537c51229e35cc1e7ad02b
liquancool <llei0111@bths.edu>
2022-09-29 18:10:28 -0400

    Added decimal format

--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("0.00");//formats to always print out 2 decimal places and an ones place
         Scanner s = new Scanner(System.in);
         System.out.println("What is the total bill?");
         double totalBill = s.nextDouble();
@@ -9,15 +12,15 @@ public class main {
         System.out.println("How many people are splitting the bill?");
         int numberOfPeople = s.nextInt();
         double totalTip = ((double)((int)(totalBill*tipPercentage+0.5)))/100;//calculates the tip and rounds it normally
-        System.out.println(totalTip);
         double billAndTip = ((double)((int)((totalTip+totalBill)*100)))/100;//calculates the total bill and tip and rounds
         double tipPerPerson = ((double)((int)((totalTip/numberOfPeople+0.005)*100)))/100;//calculates the tip per person and rounds
         double totalPerPerson = ((double)((int)((billAndTip/numberOfPeople+0.005)*100)))/100;//calculates total per person and rounds
+
         System.out.println("____________________________________________");//prints out the results
-        System.out.println("Your total tip amount is " + totalTip);
-        System.out.println("Your bill including tip is " + billAndTip);
-        System.out.println("Your tip paid per person is " + tipPerPerson);
-        System.out.println("Your total amount paid per person is " + totalPerPerson);
+        System.out.println("Your total tip amount is " + df.format(totalTip));
+        System.out.println("Your bill including tip is " + df.format(billAndTip));
+        System.out.println("Your tip paid per person is " + df.format(tipPerPerson));
+        System.out.println("Your total amount paid per person is " + df.format(totalPerPerson));
         System.out.println("____________________________________________");
     }
 }
